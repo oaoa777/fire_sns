6124bf800976b0ab53dd7d20443d88de338d804d
桜芽 <ouga20070307@gmail.com>
2025-02-09 01:53:37 +0900

    齊藤真穂削除

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'post.dart';
-//齊藤真穂
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
