508c4aa63ec81efe791a08f8f4cc3a3e7362b7d3
桜芽 <ouga20070307@gmail.com>
2025-02-09 01:56:36 +0900

    dart fix

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:fire_sns/update_page.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'post.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
